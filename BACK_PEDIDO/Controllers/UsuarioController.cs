a9a5f13918f7baef154a7cd7d938d37468108a8a
pjsa2701 <pjsa2701@gmail.com>
2024-06-14 08:29:00 -0500

    backup

---
+++ BACK_PEDIDO/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using BACK_PEDIDO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACK_PEDIDO.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class UsuarioController : Controller
+    {
+        private readonly BdPedidosContext _context;
+        public UsuarioController(BdPedidosContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Usuario> obtenerTodos() {
+            return Ok(_service.obtenerTodos());
+        }
+    }
+}
---
+++ EntityLayer/Models/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BACK_PEDIDO.Models;
+
+public partial class Empresa
+{
+    public int IdEmpresa { get; set; }
+
+    public string Ruc { get; set; } = null!;
+
+    public string RazonSocial { get; set; } = null!;
+
+    public int? EstadoIdEstado { get; set; }
+
+    public byte[]? Logo { get; set; }
+
+    public virtual Estado? EstadoIdEstadoNavigation { get; set; }
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
