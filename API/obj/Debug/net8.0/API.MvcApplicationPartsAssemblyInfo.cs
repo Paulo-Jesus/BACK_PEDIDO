5a554ee9f4d24a4f6f87bdf15bcdc33a7f3f6718
MichaelQuiroz97 <maiikquiroz53@gmail.com>
2024-07-02 08:25:10 -0500

    resolver conflictos

--- API/obj/Debug/net8.0/API.MvcApplicationPartsAssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9bdd6cd3d8fc190f4ebed653d3fa2d13e6d000")]
-[assembly: System.Reflection.AssemblyProductAttribute("API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("API")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
